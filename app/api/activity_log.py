fe76772f6858f9bd9c7771ed4a7ab81a0929014b
Alessandro Sclafani <me@scla.dev>
2024-12-17 23:40:51 +0100

    api: using urlencode

--- app/api/activity_log.py
+++ app/api/activity_log.py
@@ -1,16 +1,19 @@
 from typing import Any
 import httpx
 from app.models.activity_log import ActivityLogModel
-
+from urllib.parse import urlencode
 
 async def get_activities(
     base_url: str, api_key: str, **extra_headers: dict[str, Any]
 ) -> list[ActivityLogModel]:
-    endpoint = f"{base_url}/System/ActivityLog/Entries"
+    query = urlencode({
+        "limit": 100_000_000, # yes, this is an arbitrary big number
+        "hasUserId": "true",
+    })
+    endpoint = f"{base_url}/System/ActivityLog/Entries?{query}"
     headers = {"X-Emby-Token": api_key, **extra_headers}
 
     async with httpx.AsyncClient() as client:
-        # Send request to Jellyfin API
         response = await client.get(endpoint, headers=headers)
         response.raise_for_status()
         activity = response.json()
