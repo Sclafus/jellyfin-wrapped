32dd2f2775c5d66aa0471d2da5d15fa389a6c4cf
Alessandro Sclafani <me@scla.dev>
2024-12-17 23:41:33 +0100

    main: added filtering and aggegation per user

--- app/__main__.py
+++ app/__main__.py
@@ -1,14 +1,26 @@
+from collections import defaultdict
+
 import os
 import asyncio
 from app.api.activity_log import get_activities
+from app.models.activity_log import ActivityLogModel
 
 # Configuration
 JELLYFIN_URL = os.getenv("JELLYFIN_URL")
 API_KEY = os.getenv("API_KEY")
 
+
 async def main():
-    result = await get_activities(JELLYFIN_URL, API_KEY) # type: ignore
-    return result
+    result = await get_activities(JELLYFIN_URL, API_KEY)  # type: ignore
+    # let's filter the ones with type "VideoPlaybackStopped"
+    filtered = [act for act in result if act.type == "VideoPlaybackStopped"]
+
+    # let's aggregate the items by user
+    aggregated: dict[str, list[ActivityLogModel]] = defaultdict(list)
+    for activity in filtered:
+        aggregated[activity.user_id].append(activity)
+    return aggregated
+
 
 if __name__ == "__main__":
     asyncio.run(main())
